aa9817eb2519f90b618180b6fc3e21541a8cb594
murtazaaylak <murtazaaylak@gmail.com>
2021-11-08 11:47:53 +0300

    add: log details in treeview component

--- src/views/LogsPage.vue
+++ src/views/LogsPage.vue
@@ -1,10 +1,51 @@
 <template>
-  <div>LogsPage</div>
+  <div class="ma-5">
+    <h3 class="ml-10">Log History</h3>
+    <v-row>
+      <v-col>
+        <!-- logs display in treeview -->
+        <v-treeview dense hoverable :items="items"> </v-treeview>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'LogsPage',
+  data() {
+    return {
+      logs: JSON.parse(localStorage.getItem('localLogs')),
+    }
+  },
+  computed: {
+    //global state
+    ...mapState(['authType']),
+    // treeview items
+    items() {
+      // date display options
+      const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      }
+
+      // map logs array to structure vuetify treeview items data
+      return this.logs.map((log, i) => {
+        return {
+          id: log.date + i,
+          name: new Date(log.date).toLocaleDateString('en-US', options),
+          // treview subitems
+          children: [
+            { id: log.date + 'child1', name: 'from: ' + log.from },
+            { id: log.date + 'child2', name: 'to: ' + log.to },
+          ],
+        }
+      })
+    },
+  },
 }
 </script>
 
