5b9009252ff65e2013c72e8caf9d2e8d915f736e
murtazaaylak <murtazaaylak@gmail.com>
2021-11-08 11:34:18 +0300

    add: create logs before each route to localstorage

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import LogsPage from '../views/LogsPage.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,24 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const localLogHistory = localStorage.getItem('localLogs')
+    ? JSON.parse(localStorage.getItem('localLogs'))
+    : []
+
+  const newLog = {
+    date: new Date().getTime(),
+    from: from.path,
+    to: to.path,
+    unauthorized: store.state.authType !== 'Admin',
+  }
+
+  localLogHistory.push(newLog)
+
+  localStorage.setItem('localLogs', JSON.stringify(localLogHistory))
+  next()
 })
 
 export default router
