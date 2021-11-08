9e0e905a7aa8021a5004331f48e88c28b2b00fbd
murtazaaylak <murtazaaylak@gmail.com>
2021-11-08 23:07:03 +0300

    add: company lists coming from alpha-vantage api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,31 @@
   <div>
     <div class="ma-5" align="center">
       <v-form class="search-form" @submit.prevent="searchCompany">
+        <!-- search input -->
         <v-text-field
           v-model="search"
           label="Company Name"
           clearable
           class="search-input"
         ></v-text-field>
+        <!-- search btn -->
         <v-btn type="submit">Search</v-btn>
       </v-form>
+      <!-- found companies in alpha vantage api -->
+      <v-col class="company-list" v-if="symbols.length > 0">
+        <v-list two-line subheader dense>
+          <v-subheader>Select a company to view stock data</v-subheader>
+          <v-list-item v-for="(company, i) in symbols" :key="i">
+            <v-list-item-content>
+              <v-list-item-title>{{ company['2. name'] }}</v-list-item-title>
+              <v-list-item-subtitle>{{
+                company['4. region']
+              }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
     </div>
-    <div>{{ symbols }}</div>
   </div>
 </template>
 
@@ -21,7 +36,7 @@ export default {
   name: 'Home',
   data() {
     return {
-      search: null,
+      search: '',
     }
   },
   computed: {
@@ -41,10 +56,26 @@ export default {
   max-width: 700px;
   display: flex;
   align-items: center;
-  padding: 20px 40px;
+  padding: 20px 40px 0;
+}
+
+.company-list {
+  text-align: left;
+  max-width: 700px;
+  padding: 0 40px;
 }
 
 .search-input {
   margin-right: 10px;
+}
+
+/* to override default vuetify min-height */
+.v-list--two-line .v-list-item {
+  min-height: 30px;
+  cursor: pointer;
+}
+
+.v-list-item:hover {
+  background-color: #ebebeb;
 }
 </style>
